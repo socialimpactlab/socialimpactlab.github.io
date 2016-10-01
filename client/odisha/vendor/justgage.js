65bf82e2a0387ab9e5f064eb03594912776c9797
keithporcaro <keith.porcaro@gmail.com>
2016-10-01 16:03:59 -0400

    js bugfix

--- client/odisha/vendor/justgage.js
+++ client/odisha/vendor/justgage.js
@@ -1082,7 +1082,7 @@ function getColor(val, pct, col, noGradient, custSec) {
     gval = parseInt((cutHex(col[i])).substring(2, 4), 16);
     bval = parseInt((cutHex(col[i])).substring(4, 6), 16);
     colors[i] = {
-      pct: 1-percentage,
+      pct: percentage,
       color: {
         r: rval,
         g: gval,
